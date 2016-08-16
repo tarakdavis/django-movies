0416bfc4a70c35c10f90c89695cf36f66c4eb2c2
Peter Lawless <peterlawless223@gmail.com>
2016-08-16 16:18:27 -0400

    Changed toprated to generic view

--- movielens/movieratings/urls.py
+++ movielens/movieratings/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'movieratings'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^toprated', views.toprated, name='toprated'),
+    url(r'^toprated', views.TopRated.as_view(), name='toprated'),
     url(r'^$', views.IndexView.as_view(), name='index'),
     #url(r'^(P<pk>[0-9]+)/$', views.DetailView.as_view(), name ='movie_detail'),
     #url(r'^(P<pk>[0-9]+)/$', views.DetailView.as_view(), name ='rater_detail'),
