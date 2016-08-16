d2b85b5aae0b4c03c91b30d88294f5a3ce4432ff
Maggie Correll <maggie.correll@gmail.com>
2016-08-16 17:16:43 -0400

    csv to json converter

--- movielens/movielens/fixtures/converter.py
+++ movielens/movielens/fixtures/converter.py
@@ -1,22 +1,54 @@
 import csv
 
-def read_main_csv(csv_file):
-    with open('csv_file', 'r') as f:
-        with open ('csv_file', 'w') as w:
-            for row in reader:
-                row.replace('::', '+')
-    return
-
 
 def read_csv(csv_file, json_file):
-    read_main_csv(csv_file)
-    with open('csv_file', 'r') as f:
+    with open(csv_file, 'r') as f:
         reader = csv.reader(f, delimiter='+')
+        for row in reader:
+            with open(json_file, 'a') as json:
+                json.write('[\n')
+            with open(json_file, 'a') as json:
+                read_json(row, json_file)
+            with open(json_file, 'a') as json:
+                json.write(',\n]')
 
 
+def read_json(row, json_file):
+        if 'json_file' == 'movies.json':
+            d = {
+                'model': 'movieratings.movies',
+                'pk': int(row[0]),
+                'fields': {
+                    'title': row[1],
+                    'genre': row[2]
+                }
 
+            }
+        elif 'json_file' == 'ratings.json':
+            d = {
+                'model': 'movieratings.ratings',
+                'pk': None,
+                'fields': {
+                    'rater': int(row[0]),
+                    'movie': int(row[1]),
+                    'rating': int(row[2])
+                }
+            }
+        elif 'json_file' == 'users.json':
+            d = {
+                'models': 'movieratings.users',
+                'pk': int(row[0]),
+                'fields': {
+                    'gender': row[1],
+                    'age': int(row[2]),
+                    'occupation': int(row[3])
+                }
+            }
 
 def main():
-    csv_file = ['movies.dat', 'ratings.dat', 'users.dat']
+    csv_file = ['../../../ml-1m/movies.dat', 'ml-1m/ratings.dat', 'ml-1m/users.dat']
     json_file = ['movies.json', 'ratings.json', 'users.json']
-    read_main_csv(movie.dat)
+    read_csv(csv_file[0], json_file[0])
+
+if __name__ == '__main__':
+    main()
