8ae3df4860ca57e641f7ddc33d23ef625f9a6b88
T. Robert Ward <trironyard@icloud.com>
2016-08-16 16:13:01 -0400

    updated models

--- movielens/movieratings/models.py
+++ movielens/movieratings/models.py
@@ -5,6 +5,9 @@ class Movie(models.Model):
     title = models.CharField(max_length=200)
     genre = models.CharField(max_length=50)
 
+    def __str__(self):
+        return str(self.title, self.genre)
+
 
 class Rater(models.Model):
     gender = models.CharField(max_length=1)
@@ -12,9 +15,37 @@ class Rater(models.Model):
     occupation = models.IntegerField()
     # zipcode = models.CharField(max_length=10)
 
+    def __str__(self):
+        return str(self.id, self.gender, self.age, self.occupation)
+
+    def get_allratings_of_rater(name_id):
+        all_rater_ratings = Rater.objects.all(id=name_id)
+        return all_rater_ratings
+
 
 class Rating(models.Model):
     score = models.IntegerField()
     # timestamp = models.DateTimeField()
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     rater_id = models.ForeignKey(Rater, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ("{}.  {} - {}.  {} - {}.".format(self.score, self.movie_id, self.rater_id))
+
+    def specific_movie_rating(name_id):
+        all_movie_ratings = Rating.objects.filter(movie_id=name_id)
+        return all_movie_ratings
+
+    def get_movie_average_rating(which_one):
+        the_movie = Rating.objects.filter(movie_id=which_one)
+        agg_score = 0
+        for each in the_movie:
+            agg_score += each.score
+        try:
+            avg_rating = agg_score/len(the_movie)
+        except:
+            avg_rating = 0
+
+        if len(the_movie) < 20:
+            too_few = True
+        return (avg_rating, too_few)
