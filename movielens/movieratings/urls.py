53f2bc1bb10ee780dbc23e8bce67f73b52f27d5e
Tara Davis <tkalindavis@gmail.com>
2016-08-18 16:07:55 -0400

    Index populates.

--- movielens/movieratings/urls.py
+++ movielens/movieratings/urls.py
@@ -6,11 +6,11 @@ app_name = 'movieratings'
 urlpatterns = [
     # url(r'^$', views.index, name='index'),
     url(r'^toprated', views.TopRated.as_view(), name='toprated'),
-    url(r'^$', views.IndexView, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^allmovies', views.AllMovies.as_view(), name='all_movies'),
     url(r'^movie/(?P<pk>[0-9]+)/$', views.MovieDetail.as_view(), name='movie_detail'),
-    url(r'^rater/(?P<pk>[0-9]+)/$', views.RaterDetail.as_view(), name='rater_detail'),
-#========== REGISTRATION ================
+    url(r'^rater/(?P<pk>[0-9]+)/$', views.RaterDetail.as_view(), name='rater_detail')
+]
      # Registration URLs
 #     url(r'^accounts/register/$', views.register, name='register'),
 #     url(r'^accounts/register/complete/$', views.registration_complete, name='registration_complete'),
@@ -19,4 +19,3 @@ urlpatterns = [
 #     url(r'^accounts/logout/$', logout, name='logout'),
 #     url(r'^accounts/loggedin/$', views.loggedin, name='loggedin'),
 # ]
-#============== REGISTRATION ================
