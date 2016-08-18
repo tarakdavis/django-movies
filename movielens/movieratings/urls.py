47cb00d90e85273ac93a9ac3cfc1a18e79aa8f27
T. Robert Ward <trironyard@icloud.com>
2016-08-18 16:00:24 -0400

    added a ? and commented out the registration

--- movielens/movieratings/urls.py
+++ movielens/movieratings/urls.py
@@ -8,15 +8,15 @@ urlpatterns = [
     url(r'^toprated', views.TopRated.as_view(), name='toprated'),
     url(r'^$', views.IndexView, name='index'),
     url(r'^allmovies', views.AllMovies.as_view(), name='all_movies'),
-    url(r'^movie/(P<pk>[0-9]+)/$', views.MovieDetail.as_view(), name='movie_detail'),
-    url(r'^rater/(P<pk>[0-9]+)/$', views.RaterDetail.as_view(), name='rater_detail'),
+    url(r'^movie/(?P<pk>[0-9]+)/$', views.MovieDetail.as_view(), name='movie_detail'),
+    url(r'^rater/(?P<pk>[0-9]+)/$', views.RaterDetail.as_view(), name='rater_detail'),
 #========== REGISTRATION ================
      # Registration URLs
-    url(r'^accounts/register/$', views.register, name='register'),
-    url(r'^accounts/register/complete/$', views.registration_complete, name='registration_complete'),
-            # Auth-related URLs:
-    url(r'^accounts/login/$', login, name='login'),
-    url(r'^accounts/logout/$', logout, name='logout'),
-    url(r'^accounts/loggedin/$', views.loggedin, name='loggedin'),
-]
+#     url(r'^accounts/register/$', views.register, name='register'),
+#     url(r'^accounts/register/complete/$', views.registration_complete, name='registration_complete'),
+#             # Auth-related URLs:
+#     url(r'^accounts/login/$', login, name='login'),
+#     url(r'^accounts/logout/$', logout, name='logout'),
+#     url(r'^accounts/loggedin/$', views.loggedin, name='loggedin'),
+# ]
 #============== REGISTRATION ================
