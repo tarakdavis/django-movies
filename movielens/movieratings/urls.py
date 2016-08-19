abe54b5f6ca1c732acbbd98d72b24f760ed1f6f7
T. Robert Ward <trironyard@icloud.com>
2016-08-19 11:48:04 -0400

    wired up rest of movie detail html

--- movielens/movieratings/urls.py
+++ movielens/movieratings/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^toprated', views.TopRated.as_view(), name='toprated'),
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^allmovies', views.AllMovies.as_view(), name='all_movies'),
-    url(r'^movie/(?P<pk>[0-9]+)/$', views.MovieDetail.as_view(), name='movie_detail'),
+    url(r'^movie/(?P<pk>[0-9]+)/$', views.movie_detail, name='movie_detail'),
     url(r'^rater/(?P<pk>[0-9]+)/$', views.RaterDetail.as_view(), name='rater_detail')
 ]
      # Registration URLs
