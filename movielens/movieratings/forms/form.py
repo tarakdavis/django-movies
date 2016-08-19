c0c9c8bb531e58c77866e16c98a5e593752af494
Tara Davis <tkalindavis@gmail.com>
2016-08-19 15:11:37 -0400

    Working on registration.

---
+++ movielens/movieratings/forms/form.py
@@ -0,0 +1,9 @@
+# from django import forms
+# from .models import Rater
+#
+#
+# class RaterForm(forms.ModelForm):
+#     email = forms.EmailField(label="Email")
+#
+#     class Meta:
+#         model = Rater
