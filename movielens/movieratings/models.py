ce3b2b6d3776dbaac1ab97d16f418960d9b6aa77
T. Robert Ward <trironyard@icloud.com>
2016-08-19 19:46:13 -0400

    wrote code for num to word for Occupation in Rater

--- movielens/movieratings/models.py
+++ movielens/movieratings/models.py
@@ -29,6 +29,10 @@ class Rater(models.Model):
         total = sum(all_of_his_ratings) / all_of_his_ratings.objects.all().count()
         return total
 
+    def occupation_word(num):
+        occupation_word = ((0, 'other'), (1, 'academic/educator'), (2, 'artist'), (3, 'clerical/admin'), (4, 'college/grad student'), (5, 'customer service'), (6, 'doctor/health care'), (7, 'executive/managerial'), (8, 'farmer'), (9, 'homemaker'), (10, 'K-12 student'), (11, 'lawyer'), (12, 'programmer'), (13, 'retired'), (14, 'sales/marketing'), (15, 'scientist'), (16, 'self-employed'), (17, 'technician/engineer'), (18, 'tradesman/craftsman'), (19, 'unemployed'), (20, 'writer'))
+        return occupation_word[num]
+
 
 class Rating(models.Model):
     score = models.IntegerField()
@@ -39,6 +43,10 @@ class Rating(models.Model):
     def __str__(self):
         return ("{}.  {} - {}.  {} - {}.".format(self.score, self.movie, self.rater))
 
+
+# class OccTst(models.Rater):
+#     def cracra(num):
+#         return num*1000
 
     # def specific_movie_rating(name_id):
     #     all_movie_ratings = Rating.objects.filter(movie_id=name_id)
