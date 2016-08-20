a48e65354e336d15befa1564b2d9e48fb291566e
Peter Lawless <peterlawless223@gmail.com>
2016-08-20 08:20:19 -0400

    Added method to Rater model to find list of movies that Rater has not rated

--- movielens/movieratings/models.py
+++ movielens/movieratings/models.py
@@ -20,18 +20,8 @@ class Rater(models.Model):
     def __str__(self):
         return "{}: {}, {}, {}".format(self.id, self.gender, self.age, self.occupation)
 
-    # def get_allratings_of_rater(name_id):
-    #     all_rater_ratings = Rater.objects.all(id=name_id)
-    #     return all_rater_ratings
-    #
-    # def get_average_rating_of_this_rater(name_id):
-    #     all_of_his_ratings = Rater.get_allratings_of_rater.objects.filter(name_id)
-    #     total = sum(all_of_his_ratings) / all_of_his_ratings.objects.all().count()
-    #     return total
-    def movies_rated(self):
-        rated = Movie.objects.filter(movie__in=self.rating_set)
-        return rated
-    # def reco_top_rated(self):
+    def movies_not_rated(self):
+        return Movie.objects.exclude(id__in=self.rating_set.all())
 
 
 class Rating(models.Model):
@@ -42,39 +32,3 @@ class Rating(models.Model):
 
     def __str__(self):
         return ("Rater: {}, Movie: {}, Score: {}".format(self.rater_id, self.movie.title, self.score))
-
-
-    # def specific_movie_rating(name_id):
-    #     all_movie_ratings = Rating.objects.filter(movie_id=name_id)
-    #     return all_movie_ratings
-    #
-    # def get_movie_average_rating(which_one):
-    #     too_few = False
-    #     the_movie = Rating.objects.filter(movie_id=which_one)
-    #     agg_score = 0
-    #     for each in the_movie:
-    #         agg_score += each.score
-    #     try:
-    #         avg_rating = agg_score/len(the_movie)
-    #     except:
-    #         avg_rating = 0
-    #
-    #     if len(the_movie) < 20:
-    #         too_few = True
-    #     return (avg_rating, too_few)
-    #
-    # def get_top_rated_movies(num):
-    #         averages = []
-    #         top = []
-    #         top_movies = Movie.objects.all().count()
-    #         for i in range(top_movies):
-    #             avg, not_enough_reviews = Rating.get_movie_average_rating(i+1)
-    #             if not_enough_reviews is False:
-    #                 averages.append((avg, i+1))
-    #             print("\n"*50)
-    #             c = (i+1) / 1683
-    #             print("Percentage complete:  ", c, "%")
-    #         averages.sort(reverse=True)
-    #         for i in range(num):
-    #             top.append(averages[i])
-    #         return top      # returns list of TUPLES !
