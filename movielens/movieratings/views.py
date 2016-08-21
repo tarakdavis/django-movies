a10c94f4c9fb77e0af4d9b396a4881a49430392f
Peter Lawless <peterlawless223@gmail.com>
2016-08-21 16:31:26 -0400

    Reduced repetition of queries in MovieDetail view to speed up page generation

--- movielens/movieratings/views.py
+++ movielens/movieratings/views.py
@@ -96,16 +96,21 @@ class MovieDetail(generic.DetailView):
         ctx = super(MovieDetail, self).get_context_data(*args, **kwargs)
         ratings = self.get_object().rating_set.all()
         ctx['avg_rating'] = ratings.aggregate(Avg('score'))['score__avg']
-        ctx['five'] = len(ratings.filter(score=5))
-        ctx['four'] = len(ratings.filter(score=4))
-        ctx['three'] = len(ratings.filter(score=3))
-        ctx['two'] = len(ratings.filter(score=2))
-        ctx['one'] = len(ratings.filter(score=1))
-        ctx['r_five'] = ratings.filter(score=5)
-        ctx['r_four'] = ratings.filter(score=4)
-        ctx['r_three'] = ratings.filter(score=3)
-        ctx['r_two'] = ratings.filter(score=2)
-        ctx['r_one'] = ratings.filter(score=1)
+        one = ratings.filter(score=1)
+        two = ratings.filter(score=2)
+        three = ratings.filter(score=3)
+        four = ratings.filter(score=4)
+        five = ratings.filter(score=5)
+        ctx['five'] = len(five)
+        ctx['four'] = len(four)
+        ctx['three'] = len(three)
+        ctx['two'] = len(two)
+        ctx['one'] = len(one)
+        ctx['r_five'] = five
+        ctx['r_four'] = four
+        ctx['r_three'] = three
+        ctx['r_two'] = two
+        ctx['r_one'] = one
         return ctx
 
 # class MovieGenreDetail(generic.DetailView):
@@ -132,9 +137,6 @@ class RaterDetail(generic.DetailView):
         movies = rater.movies_not_rated().annotate(num_rat=Count('rating')).filter(num_rat__gte=50)
         toprated = movies.annotate(avg_rating=Avg('rating__score')).order_by('-avg_rating')
         preferred_genre = toprated.filter(genre__contains=favorite_genres[0])
-
-        #|Q(genre__contains=favorite_genres[1])|Q(genre__contains=favorite_genres[2]))
-
         occupation = rater.occupation_word()
         ctx['occupation'] = occupation
         ctx['toprated'] = toprated[:5]
